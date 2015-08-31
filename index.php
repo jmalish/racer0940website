0972ff29e95d94e93d8b543954804f2ebfa1f7f6
jmalish <jordanmalish@gmail.com>
2015-08-31 14:01:17 -0400

    Delete old junk

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include_once('./templates/header.php'); ?>
-
-
-<body>
-    <a>This is the body</a>
-</body>
-
-
-<?php include_once('./templates/footer.php'); ?>
--- templates/footer.php
+++
@@ -1,4 +0,0 @@
-    <footer>
-        <a>this is the footer</a>
-    </footer>
-</div> <!-- end of wrapper div -->
