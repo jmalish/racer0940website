9cfe5a1c0f20a6aa5fc635078e984adc179c4cd4
jmalish <jordanmalish@gmail.com>
2015-09-01 14:51:40 -0400

    stuff was did

--- server.js
+++ server.js
@@ -2,33 +2,56 @@
 // load prerequisites
 var express = require('express');
 var app = express();
+var request = require('request');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
 // setting view engine to use ejs
 app.set('view engine', 'ejs');
+
+// telling server to use public folder
+app.use(express.static(__dirname + '/public'));
 
 ///////////
 // PAGES //
 ///////////
 // index page
 app.get('/', function(req, res) {
-    var drinks = [
-        { name: 'Drink1', drunkness: 5},
-        { name: 'Drink2', drunkness: 15},
-        { name: 'Drink3', drunkness: 9},
-    ];
-    var tagline = "This is a tagline, used for tagging stuff, maybe on lines.";
+    var xinput = true;
 
-    res.render('pages/index', {
-        drinks: drinks,
-        tagline: tagline
+    // get json from twitch
+    request({
+        url: 'https://api.twitch.tv/kraken/streams/racer0940',
+        json: true
+    }, function(error, response, body){
+        // if twitch says the channel is not live, hide the player
+        if (body.stream === null) {
+            res.render('pages/', {
+                title:'racer0940.com :: Home',
+                isLive: false
+            });
+        } else { // show the player
+            res.render('pages/', {
+                title:'racer0940.com :: Home',
+                isLive: true
+            });
+        }
     });
 });
 
+//youtube api: https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUkUuHpGDYHTW-I3Wvlwpf4g&key=
+
 // about page
 app.get('/about', function (req, res) {
-    res.render('pages/about');
+    res.render('pages/about', {
+        title:'racer0940.com :: About'
+    });
 });
 
+// testing page
+app.get('/test', function (req, res) {
+
+});
 
 //////////////////
 // Server Stuff //
