d461f2be5c85a899ff39afaac8322046207ae5ac
jmalish <jordanmalish@gmail.com>
2015-09-08 09:08:54 -0400

    Change port to port 80

--- server.js
+++ server.js
@@ -332,5 +332,5 @@ app.get('*', function (req, res) {
 //////////////////
 // Server Stuff //
 //////////////////
-app.listen(8080);
-console.log('Server running on port 8081');
+app.listen(80);
+console.log('Server running on port 80');
