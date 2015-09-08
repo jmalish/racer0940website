91ebe346fee65de3cb2954b66761307ed8bde5db
jmalish <jmalish@users.noreply.github.com>
2015-09-08 09:13:45 -0400

    Change port back to 8080

--- server.js
+++ server.js
@@ -332,5 +332,5 @@ app.get('*', function (req, res) {
 //////////////////
 // Server Stuff //
 //////////////////
-app.listen(80);
-console.log('Server running on port 80');
+app.listen(8080);
+console.log('Server running on port 8080');
