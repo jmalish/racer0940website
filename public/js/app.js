6d4e711e0a5abcbea580340e1cd4a576ce3d7f0e
jmalish <jordanmalish@gmail.com>
2015-09-26 15:24:55 -0400

    Make iracingTracks use angular, still WIP2

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,16 @@
-var racerApp = angular.module('racerApp', []);
+var racerApp = angular.module('racerApp', ['checkFilter']);
 
 racerApp.controller('trackListCtrl', function($scope, $http) {
     $http.get('/api/tracks').success(function(data) {
         $scope.tracks = data;
     });
 
-    $scope.defaultOnly = {
-        value1 : '0'
-    }
+    $scope.defaultCheckChange = function() {
+        if ($scope.defaultOnly) {
+            $scope.defaultOnlytest = "0"; // true
+        } else {
+            $scope.defaultOnlytest = "no";
+        }
+    };
 });
+
---
+++ public/js/filters.js
@@ -0,0 +1,5 @@
+angular.module('checkFilter', []).filter('checkmark', function() {
+    return function(input) {
+        return input ? '\u2713' : '\u2718';
+    };
+});
