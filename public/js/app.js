38e4f51d7828f637bb0131e24a9f0d70eb030cde
jmalish <jordanmalish@gmail.com>
2015-09-30 14:37:24 -0400

    Add routing to tracks page

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,17 @@
 var racerApp = angular.module('racerApp', [
     'checkFilter',
+    'ngRoute',
     'racerControllers'
 ]);
 
+racerApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/tracks', {
+                templateUrl: 'partials/track-list.html',
+                controller: 'trackListCtrl'
+            }).
+            otherwise({
+                redirectTo: '/tracks'
+            });
+    }]);
