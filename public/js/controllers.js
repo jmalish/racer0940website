bcae700250ab4c11a7da0110e072e89357c97c57
jmalish <jordanmalish@gmail.com>
2015-10-01 16:23:10 -0400

    Change random race page over to angular

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -21,3 +21,29 @@ racerControllers.controller('carListCtrl', function($scope, $http) {
         $scope.cars = data;
     });
 });
+
+// random race page
+racerControllers.controller('randomRaceCtrl', function($scope, $http) {
+    $scope.reloadPage = function() {
+        // get configs
+        $http.get('/api/configs').success(function(data) {
+            $scope.tracks = data;
+
+            $scope.randTrack = getRandomNum(data.length);
+        });
+
+        // get cars
+        $http.get('/api/cars').success(function(data) {
+            $scope.cars = data;
+
+            $scope.randCar = getRandomNum(data.length);
+        });
+
+        // get random number
+        getRandomNum = function(max){
+            return Math.floor((Math.random()*max)+1);
+        };
+    };
+
+    $scope.reloadPage();
+});
