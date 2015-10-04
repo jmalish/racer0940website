25a39c4c7acd8fba207a85861edefbba84937b8e
racer0940 <jordanmalish@gmail.com>
2015-10-04 17:33:36 -0400

    Clean up some unused files, add dynamic titles to pages

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,34 +3,42 @@ var racerControllers = angular.module('racerControllers', []);
 
 
 // tracks
-racerControllers.controller('trackListCtrl', function($scope, $http) {
+racerControllers.controller('trackListCtrl', function($scope, $http, $rootScope) {
     $http.get('/api/tracks').success(function(data) {
         $scope.tracks = data;
     });
+
+    $rootScope.pageTitle = "Track List :: racer0940.com";
 });
 
-racerControllers.controller('trackDetailsCtrl', function($scope, $http, $routeParams) {
+racerControllers.controller('trackDetailsCtrl', function($scope, $http, $routeParams, $rootScope) {
     $http.get('/api/configs/' + $routeParams.trackShortname).success(function(data) {
         $scope.configs = data;
+
+        $rootScope.pageTitle = "Track Detail for " + data[0].track_name + " :: racer0940.com";
     });
 });
 
 // configs page
-racerControllers.controller('configListCtrl', function($scope, $http) {
+racerControllers.controller('configListCtrl', function($scope, $http, $rootScope) {
     $http.get('/api/configs').success(function(data) {
         $scope.configs = data;
     });
+
+    $rootScope.pageTitle = "All Track Configurations :: racer0940.com";
 });
 
 // cars
-racerControllers.controller('carListCtrl', function($scope, $http) {
+racerControllers.controller('carListCtrl', function($scope, $http, $rootScope) {
     $http.get('/api/cars').success(function(data) {
         $scope.cars = data;
     });
+
+    $rootScope.pageTitle = "Cars :: racer0940.com";
 });
 
 // random race page
-racerControllers.controller('randomRaceCtrl', function($scope, $http) {
+racerControllers.controller('randomRaceCtrl', function($scope, $http, $rootScope) {
     $scope.reloadPage = function() {
         // get configs
         $http.get('/api/configs').success(function(data) {
@@ -51,6 +59,8 @@ racerControllers.controller('randomRaceCtrl', function($scope, $http) {
             return Math.floor((Math.random()*max)+1);
         };
     };
+
+    $rootScope.pageTitle = "Random Race Generator :: racer0940.com";
 
     $scope.reloadPage();
 });
